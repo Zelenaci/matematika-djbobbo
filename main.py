48d2229c5611adc49559999b9d167eec858fc0ee
djbobbo <bohous.steier@seznam.cz>
2022-01-17 14:14:30 +0100

    co jsem udelal ve skole

--- main.py
+++ main.py
@@ -2,20 +2,8 @@
 
 from os.path import basename, splitext
 import tkinter as tk
-
+import random
 # from tkinter import ttk
-
-
-class About(tk.Toplevel):
-    def __init__(self, parent):
-        super().__init__(parent, class_=parent.name)
-        self.config()
-
-        btn = tk.Button(self, text="Konec", command=self.close)
-        btn.pack()
-
-    def close(self):
-        self.destroy()
 
 
 class Application(tk.Tk):
@@ -32,10 +20,47 @@ class Application(tk.Tk):
         self.btn.pack()
         self.btn2 = tk.Button(self, text="About", command=self.about)
         self.btn2.pack()
+    
+    
+    def generuj(self):
+        funkce = random.choice([self.plus,self.minus,self.krat,self.deleno])
+        funkce()
+        if funkce == self.plus:
+            print("plus")
+        if funkce == self.minus:
+            print("minus")            
+        if funkce == self.deleno:
+            print("deleno")
+        if funkce == self.krat:
+            print("krat")
 
+
+    def plus(self):
+        self.cisloA = random.randint(1,99)
+        self.cisloB = random.randint(1,100 - self.cisloA)
+        self.vysledek = self.cisloA + self.cisloB
+        self.lbl.config(text="+")
+
+    def minus(self):
+        self.lbl.congifg(text="-")
+        self.cisloA = random.randint(1,99)
+        self.cisloB = random.randint(1,99)
+        self.vysledek = self.cisloA - self.cisloB
+   
+    def deleno(self):
+        self.vysledek = random.randint(1, 9)
+        self.cisloB = random.randint(1, 9)
+        self.cisloA = self.vysledek * self.cisloB
+        self.lbl.congifg(text="/")
+   
+    def krat(self):
+        self.lbl.congifg(text="*")
+        self.cisloA = random.randint(1,99)
+        self.cisloB = random.randint(1,99)
+       
+   
     def about(self):
-        window = About(self)
-        window.grab_set()
+        self.generuj()
 
     def quit(self, event=None):
         super().quit()
